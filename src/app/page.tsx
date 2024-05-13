44983a53c00ba09a7c1ed18bf6b45a9baf5f5a58
Shrey Vichare <smashshrey@gmail.com>
2024-05-13 12:12:28 +0530

    Why Choose Us

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import FeaturedCourse from "@/components/FeaturedCourse";
 import HeroSection from "@/components/HeroSection";
+import WhyChooseUs from "@/components/WhyChooseUs";
 
 export default function Home() {
   return (
     <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white">
       <HeroSection />
       <FeaturedCourse />
+      <WhyChooseUs />
     </main>
   );
 }
